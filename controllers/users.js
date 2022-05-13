8b300cc0db9d41072c23d74d35818ed45d003bc1
Felix <feelix.nqn@live.com>
2022-05-13 15:54:55 -0300

    createUser done

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,29 @@
 const users = [];
 
+const User = require('../models/user-model');
+
+const createUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        const user = new User({
+            name,
+            email,
+            password,
+        });
+
+        await user.save();
+
+        res.status(201).send({
+            message: 'User created successfully',
+        });
+    } catch (error) {
+        res.status(500).send({
+            message: 'User creation failed',
+        });
+    }
+};
+
 const getUsers = async (req, res) => {
     res.status(201).send({
         message: 'Users retrieved successfully',
@@ -9,12 +33,6 @@ const getUsers = async (req, res) => {
 const getUserById = async (req, res) => {
     res.status(201).send({
         message: 'User retrieved successfully',
-    });
-};
-
-const createUser = async (req, res) => {
-    res.status(201).send({
-        message: 'User created successfully',
     });
 };
 
