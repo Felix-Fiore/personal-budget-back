ecc66bf4d10993f95f87d92adf19cbc1d62d760f
Felix <feelix.nqn@live.com>
2022-05-18 15:05:42 -0300

    updateOperation and deleteOpearion done

--- controllers/operations.js
+++ controllers/operations.js
@@ -52,16 +52,61 @@ const createOperation = async (req, res) => {
     }
 };
 
-const updateOperation = (req, res) => {
-    res.status(201).send({
-        message: 'Operation updated successfully',
-    });
+const updateOperation = async (req, res) => {
+    const operationId = req.params.id;
+
+    try {
+        const operation = await Operation.findById(operationId);
+
+        if (!operation) {
+            res.status(404).send({
+                message: 'Operation not found',
+            });
+        }
+
+        const newOperation = { ...req.body };
+
+        // { new: true } tells mongoose to return the updated document
+        const updatedOperation = await Operation.findByIdAndUpdate(
+            operationId,
+            newOperation,
+            { new: true }
+        );
+
+        res.json({
+            message: 'Operation updated successfully',
+            updatedOperation,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            message: 'Operation update failed',
+        });
+    }
 };
 
-const deleteOperation = (req, res) => {
-    res.status(201).send({
-        message: 'Operation deleted successfully',
-    });
+const deleteOperation = async (req, res) => {
+    const operationId = req.params.id;
+
+    try {
+        const operation = await Operation.findById(operationId);
+
+        if (!operation) {
+            res.status(404).send({
+                message: 'Operation not found',
+            });
+        }
+
+        await Operation.findByIdAndDelete(operationId);
+
+        res.json({
+            message: 'Operation deleted successfully',
+        });
+    } catch {
+        res.status(500).send({
+            message: 'Operation deletion failed',
+        });
+    }
 };
 
 module.exports = {
