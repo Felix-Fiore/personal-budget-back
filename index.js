6b9976b45537b616b015089413ed463bafe8fbbd
Felix <feelix.nqn@live.com>
2022-06-23 19:14:01 -0300

    Users schema done

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const { sequelize } = require('./database/config');
+
 require('dotenv').config();
+require('./models/users');
 
 // Create a new express application instance
 const app = express();
 
 // Database connection
-//sequelize;
+const sequelizeStart = async () => {
+  try {
+    await sequelize.sync();
+    // Start the server
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+sequelizeStart();
 
 // CORS
 app.use(cors());
@@ -18,18 +32,3 @@ app.use(express.json());
 // Routs
 app.use('/api/users', require('./routes/users'));
 app.use('/api/operations', require('./routes/operations'));
-
-// Start the server
-const sequelizeStart = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-};
-
-sequelizeStart();
---
+++ models/Users.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('../database/config');
+
+const users = sequelize.define('users', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = users;
