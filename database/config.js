ab588df3eec3a67838ab62454c403a5934b4db63
Felix <feelix.nqn@live.com>
2022-07-18 22:11:43 -0300

    Specifying dialect for sequelize

--- database/config.js
+++ database/config.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_DATABASE_URI);
+const sequelize = new Sequelize(process.env.DB_DATABASE_URI, {
+  dialect: 'postgres',
+});
 
 module.exports = { sequelize };
