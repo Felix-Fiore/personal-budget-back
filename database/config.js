793aead7be68f0b442ffb74e0373faf08e2a0c9a
Felix <feelix.nqn@live.com>
2022-07-19 16:12:41 -0300

    Connection succesfully done with heroku config vars

--- database/config.js
+++ database/config.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
@@ -9,23 +9,5 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
   },
 });
-/* const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  },
-
-  {}
-); */
 
 module.exports = { sequelize };
