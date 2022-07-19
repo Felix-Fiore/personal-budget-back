12ee549c3d83d2e312625518448c6f2b217318bf
Felix <feelix.nqn@live.com>
2022-07-18 22:40:30 -0300

    .

--- database/config.js
+++ database/config.js
@@ -1,7 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_DATABASE_URI, {
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+  'postgres://yfbtenrkhideei:133634341fcc7ef5b0907acb9aa6db537ad79b9cd5855d82aa6f4d33d45fff5a@ec2-34-235-198-25.compute-1.amazonaws.com:5432/da9f2fim3gvg8b',
+  {
+    dialect: 'postgres',
+  }
+);
 
 module.exports = { sequelize };
