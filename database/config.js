87e19d639241a80aecde8f6c43b0d2af6d1b7615
Felix <feelix.nqn@live.com>
2022-07-18 23:25:14 -0300

    Loggin to database by credentials

--- database/config.js
+++ database/config.js
@@ -1,8 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  'postgres://yfbtenrkhideei:133634341fcc7ef5b0907acb9aa6db537ad79b9cd5855d82aa6f4d33d45fff5a@ec2-34-235-198-25.compute-1.amazonaws.com:5432/da9f2fim3gvg8b',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'postgres',
   }
 );
