06a092ace561ec0cb65dbca0ffc18769c88ebdbc
Felix <feelix.nqn@live.com>
2022-07-19 12:44:45 -0300

    .

--- database/config.js
+++ database/config.js
@@ -6,8 +6,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'postgres',
+    //port: process.env.DB_PORT,
+    //dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
