f1c48d959ea477e0ca3a6be114b8117e5e84143f
Felix <feelix.nqn@live.com>
2022-07-18 22:35:54 -0300

    conditional port added for heroku

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const { sequelize } = require('./database/config');
 
 require('./models/Users');
 require('./models/Operations');
+
+const PORT = process.env.PORT || 4001;
 // Create a new express application instance
 const app = express();
 
@@ -16,8 +18,8 @@ const sequelizeStart = async () => {
   try {
     await sequelize.sync({ alter: false });
     // Start the server
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on port ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   } catch (error) {
     console.error('Unable to connect to the database:', error);
